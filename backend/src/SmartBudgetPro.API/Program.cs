079a50c1d325a817eaac1d9af071f7e05e38bf85
DevSamuelBrito <samuelbrito.dev@gmail.com>
2026-03-10 14:47:21 -0300

    feat(backend): adiciona middleware global para captura de erro

--- backend/src/SmartBudgetPro.API/Program.cs
+++ backend/src/SmartBudgetPro.API/Program.cs
@@ -1,3 +1,4 @@
+using SmartBudgetPro.API.Middlewares;
 using SmartBudgetPro.Application;
 using SmartBudgetPro.Infrastructure;
 
@@ -17,6 +18,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.MapControllers();
 
 app.Run();
