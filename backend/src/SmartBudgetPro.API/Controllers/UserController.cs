91dab611aee7668618621fc2e4d90630d8045e37
DevSamuelBrito <samuelbrito.dev@gmail.com>
2026-03-11 22:50:15 -0300

    feat(backend): cria endpoint para listar todos os users

--- backend/src/SmartBudgetPro.API/Controllers/UserController.cs
+++ backend/src/SmartBudgetPro.API/Controllers/UserController.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using SmartBudgetPro.Application.UseCases.User.CreateUser;
+using SmartBudgetPro.Application.UseCases.User.GetAllUsers;
 
 namespace SmartBudgetPro.API.Controllers
 {
     [ApiController]
     [Route("api/users")]
-    public class UserController(CreateUserUseCase createUserUseCase) : ControllerBase
+    public class UserController(CreateUserUseCase createUserUseCase, GetAllUsersUseCase getAllUsersUseCase) : ControllerBase
     {
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var output = await getAllUsersUseCase.ExecuteAsync();
+            return Ok(output);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateUserUseCaseInput input)
         {
