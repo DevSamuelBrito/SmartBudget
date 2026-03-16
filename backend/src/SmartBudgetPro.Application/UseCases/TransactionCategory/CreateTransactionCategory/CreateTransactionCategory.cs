f0470babcf92c6a294804814d674ddcd7abcf072
DevSamuelBrito <samuelbrito.dev@gmail.com>
2026-03-15 22:57:29 -0300

    WIP(backend): Criando controller para Transaction e useCase de CreateTransactionCategory

---
+++ backend/src/SmartBudgetPro.Application/UseCases/TransactionCategory/CreateTransactionCategory/CreateTransactionCategory.cs
@@ -0,0 +1,8 @@
+﻿using SmartBudgetPro.Application.Interfaces;
+
+namespace SmartBudgetPro.Application.UseCases.TransactionCategory.CreateTransactionCategory
+{
+    public class CreateTransactionCategory(ITransactionCategoryRepository transactionCategoryRepository)
+    {
+    }
+}
