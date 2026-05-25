8238280ce17dd78e02e269fab3b9a110d2bdfd0c
DevSamuelBrito <samuelbrito.dev@gmail.com>
2026-05-25 14:13:03 -0300

    feat(backend): improve transaction category update logic and add existing category check

--- backend/src/SmartBudgetPro.Application/UseCases/TransactionCategory/UpdateTransactionCategory/UpdateTransactionCategoryUseCase.cs
+++ backend/src/SmartBudgetPro.Application/UseCases/TransactionCategory/UpdateTransactionCategory/UpdateTransactionCategoryUseCase.cs
@@ -4,12 +4,18 @@ namespace SmartBudgetPro.Application.UseCases.TransactionCategory.UpdateTransact
 {
     public class UpdateTransactionCategoryUseCase(ITransactionCategoryRepository transactionCategoryRepository)
     {
-        public async Task ExecuteAsync (UpdateTransactionCategoryUseCaseInput input)
+        public async Task ExecuteAsync(UpdateTransactionCategoryUseCaseInput input)
         {
             var category = await transactionCategoryRepository.GetByIdAsync(input.Id);
 
             if (category == null)
                 throw new InvalidOperationException("Transaction category not found.");
+
+            var existingCategory = await transactionCategoryRepository.GetByNameAsync(category.UserId, input.Name);
+
+            if (existingCategory is not null && existingCategory.Id != input.Id)
+                throw new InvalidOperationException("A category with the same name already exists for this user.");
+
 
             category.Rename(input.Name);
             category.ChangeIcon(input.Icon);
